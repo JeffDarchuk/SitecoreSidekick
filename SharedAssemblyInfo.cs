389ccb8bbe0533b8859657b1d7a9ffe3d08bbf4f
jeffdarchuk <jdarchuk@connectivedx.com>
2017-05-22 07:18:20 -0700

    tweaking positioning and getting ready to cut a release

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -11,4 +11,4 @@ using System.Runtime.InteropServices;
 [assembly: CLSCompliant(false)]
 [assembly: AssemblyVersion("1.2.0.0")]
 [assembly: AssemblyFileVersion("1.2.0.0")]
-[assembly: AssemblyInformationalVersion("1.2.0-alpha8")]
+[assembly: AssemblyInformationalVersion("1.2.0")]

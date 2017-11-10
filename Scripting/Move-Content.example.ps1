bff9d0fe14171bc71a6b6ba144ff5e78c8470600
Jeff <sporkums@gmail.com>
2017-11-10 09:35:24 -0800

    Update Move-Content.example.ps1

--- Scripting/Move-Content.example.ps1
+++ Scripting/Move-Content.example.ps1
@@ -1,17 +1,17 @@
-#EXAMPLE USEAGES OF THE MOVE-CONTENT POWERSHELL MODULE
-
-$ScriptPath = Split-Path $MyInvocation.MyCommand.Path
-#IMPORT MODULE
-Import-Module $ScriptPath\Move-Content.psm1
-
+Import-Module -Name SitecoreSidekick -Force -Verbose
+$params = @{
+    LocalUrl = "http://demo2"
+    RemoteUrl = "http://demo"
+    SharedSecret = "3a4e9aad-5d61-42ae-b262-de0e13f3b576"
+    EventDisabler = $true
+    BulkUpdate = $true
+    PullParent = $true
+}
 #MAKE THE CONTENT FROM ONE SERVER MATCH THE OTHER EXACTLY
-move-content -LocalUrl 'https://serverWhoWantsContent' -RemoteUrl 'https://serverWhoHasContent' -SharedSecret 'Secret key defined for content migrator on both local and remote sitecore sites' -RootIds '{0DE95AE4-41AB-4D01-9EB0-67441B7C2450}' -Children -Overwrite -PullParent -EventDisabler -BulkUpdate -RemoveLocalNotInRemote
-
+Move-Content @params -RootId '{0DE95AE4-41AB-4D01-9EB0-67441B7C2450}' -Children -Overwrite -RemoveLocalNotInRemote
 #MOVE ALL CONTENT FROM ONE SERVER TO THE OTHER AND ALLOWING UNIQUE ITEMS AT THE CONSUMER
-move-content -LocalUrl 'https://serverWhoWantsContent' -RemoteUrl 'https://serverWhoHasContent' -SharedSecret 'Secret key defined for content migrator on both local and remote sitecore sites' -RootIds '{0DE95AE4-41AB-4D01-9EB0-67441B7C2450}' -Children -Overwrite -PullParent -EventDisabler -BulkUpdate
-
+Move-Content @params -RootId '{0DE95AE4-41AB-4D01-9EB0-67441B7C2450}' -Children -Overwrite
 #MOVE ONLY NEW CONTENT FROM ONE SERVER TO THE OTHER WITHOUT UPDATING MODIFIED
-move-content -LocalUrl 'https://serverWhoWantsContent' -RemoteUrl 'https://serverWhoHasContent' -SharedSecret 'Secret key defined for content migrator on both local and remote sitecore sites' -RootIds '{0DE95AE4-41AB-4D01-9EB0-67441B7C2450}' -Children -PullParent -EventDisabler -BulkUpdate
-
+Move-Content @params -RootId '{0DE95AE4-41AB-4D01-9EB0-67441B7C2450}' -Children
 #ONLY MOVE THE INDIVIDUAL ITEMS AND NOT CHILDREN
-move-content -LocalUrl 'https://serverWhoWantsContent' -RemoteUrl 'https://serverWhoHasContent' -SharedSecret 'Secret key defined for content migrator on both local and remote sitecore sites' -RootIds '{0DE95AE4-41AB-4D01-9EB0-67441B7C2450}' -Overwrite -PullParent -EventDisabler -BulkUpdate
+Move-Content @params -RootId '{0DE95AE4-41AB-4D01-9EB0-67441B7C2450}' -Overwrite
